1814ddfe26ece47b87ed5c74c328306ad5070f7e
l3v3nshtain <mikhvepriail.com>
2024-11-06 23:41:12 +0300

    nachalo zbt2 (((

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-// Токен бота, который ты получил от BotFather
-const BOT_TOKEN = '7982544171:AAHwRRUebX2gy7Y43n6hI2CIsgAk4TMgq5w'; 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const keyboard = {
+    inline_keyboard: [[{
+      text: 'Открыть SBT Manager',
+      web_app: { url: WEB_APP_URL }
+    }]]
+  };
 
-// Массив с ID пользователей, которым нужно отправить сообщение
-const USER_IDS = ['865178331', '1322759567', '7130123407', '1055247663', '493002213', '1067502180', '377584990', '1557169525', '382774524', '139860410', '1104635743', '6951716788', '676374146', '305768529', '444902492', '325905836', '1708940841', '446994336', '2130803801', '1117906408', '671591900', '678418106', '384287768']; 
-
-// Ссылка на твоё приложение
-const WEB_APP_URL = 'https://notbez.github.io/SBT/';
-
-// Функция для отправки сообщения с кнопкой Web App
-function sendWebAppButton(chatId) {
-    axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
-        chat_id: chatId,
-        text: 'Открыть SBT Manager',
-        reply_markup: {
-            inline_keyboard: [[{
-                text: 'Открыть SBT Manager',
-                web_app: { url: WEB_APP_URL }
-            }]]
-        }
-    })
-    .then(response => {
-        console.log('Сообщение отправлено:', response.data);
-    })
-    .catch(error => {
-        console.error('Ошибка при отправке сообщения:', error);
-    });
-}
-
-// Отправляем сообщение всем пользователям из массива USER_IDS
-USER_IDS.forEach(userId => {
-    sendWebAppButton(userId);
+  bot.sendMessage(chatId, 'Нажми кнопку ниже для доступа к SBT Manager', {
+    reply_markup: keyboard
+  });
 });
-
